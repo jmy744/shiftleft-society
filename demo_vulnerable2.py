3cb96accd31a1f5a57c9ef2acfcfe8d7eb4dac3d
jmy744 <45516658+jmy744@users.noreply.github.com>
2026-07-01 21:45:18 +0800

    Fix formatting in fetch_all_records function
    Added a space before the return statement to ensure proper formatting.

--- demo_vulnerable2.py
+++ demo_vulnerable2.py
@@ -4,4 +4,4 @@ def run_diagnostic(command):
     os.system("ping -c 1 " + command)
 
 def fetch_all_records(db):
-    return db.execute("SELECT * FROM transactions").fetchall()
+    return db.execute("SELECT * FROM transactions").fetchall() 
